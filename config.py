867c79f977bd0496e2e47fbd02c3512f147b8c0f
Chew3488 <95432503+SeventyForce@users.noreply.github.com>
2023-04-12 16:39:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cql6qopjwdm7.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "leowcheeyew-employee"
+customregion = "us-east-1"
